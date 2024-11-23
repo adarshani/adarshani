0555473f23f76ecfd9580b3915661ae2793a1bb0
adarshani <adarshanipatra@github.com>
2024-11-23 21:23:31 +0530

    greetings

---
+++ javascript/lesson 79/data types/script.js
@@ -0,0 +1,20 @@
+var a = 8
+var b = 10
+var c = null
+var d; //undefined
+{
+    let a = 8;
+
+    document.write("The value of a is " + a);
+    document.write("<br>")
+    const b = 10;
+
+}
+document.write(" a is equal to be " + (a == b))
+document.write("<br>")
+document.write("b is " + b)
+document.write("<br>")
+document.write("c is " + c)
+document.write("<br>")
+document.write("d is " + d)
+document.write("<br>")
