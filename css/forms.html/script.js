3554dda105b18ba71a0eaa2ba0bb01cff64e6ce4
adarshani <adarshanipatra@github.com>
2024-12-28 11:38:12 +0530

    to do list h.w

---
+++ css/forms.html/script.js
@@ -0,0 +1,5 @@
+function submit(){
+    var num1 = document.getElementById("a").value
+    document.getElementById("result").innerHTML = num1*10
+}
+ 
---
+++ javascript/App-developmwnt using js/lesson 90/introduction to js app.js
@@ -0,0 +1,28 @@
+var choice = prompt(" welcome to area calculator \n enter you choice \n1 area of rectancle \n2 area of triangle \n3 area of circle \n4 area of paralleogram")
+if(choice=="1"){
+    var l = prompt("enter the length");
+    var b = prompt("enter the breadth");
+    var result = Number(l)*Number(b)
+    alert("the area is " + result )
+
+}
+if (choice == "2") {
+    var h = prompt("enter the height");
+    var ba = prompt("enter the base");
+    var result = Number(h) * Number(ba)/2
+    alert("the area is " + result)
+
+}
+if (choice == "3") {
+    var r = prompt("enter the radius");
+    var result = 3.14*Number(r)*Number(r)
+    alert("the are is " + result)
+
+}
+if (choice == "4") {
+    var h = prompt("enter the height");
+    var cb = prompt("enter the corresponding base");
+    var result = Number(h) * Number(cb)
+    alert("the are is " + result)
+
+}
