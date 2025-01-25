54efe415b66aea0f915d9b299fc60a644c23e65a
adarshani <adarshanipatra@github.com>
2025-01-25 21:45:20 +0530

    py ls1

---
+++ python/pyton basic/lesson1/statements&comenting.py
@@ -0,0 +1,12 @@
+print("Welcome to Python")
+
+#about my name
+print("My name is : Adarshani","today I am Gonna say about Python")
+
+#about python
+print("python is a programming language")
+print("Python is easy to learn")
+print("Python is open source")
+
+#some sums
+print("add 3 and 5",3+5)
