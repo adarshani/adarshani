f122a093c562f63798c312a9b1b74b277e210dcc
adarshani <adarshanipatra@github.com>
2025-01-27 19:35:21 +0530

    BIRTHDAY SORTING

---
+++ python/pyton basic/lesson2/h.w.py
@@ -0,0 +1,14 @@
+print("Birthdays of 5 friends")
+
+
+a="30-3-2012"
+print("Naksh -",a)
+
+b="27-1-2012"
+print("sonu -",b)
+
+c="12-6-2012"
+print("Adu -",c)
+
+d="28-1-2012"
+print("Friend1 -",d)
---
+++ python/pyton basic/lesson2/keywords.py
@@ -0,0 +1,5 @@
+import keyword
+
+#all python language keywords
+print("python keywords are..\n")
+print(keyword.kwlist)
