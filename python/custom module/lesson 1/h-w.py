643bceba4ebb65e44b82bd429b430b0e92e67c40
adarshani <adarshanipatra@github.com>
2025-02-01 21:33:36 +0530

    ipper in py

---
+++ python/custom module/lesson 1/h-w.py
@@ -0,0 +1,3 @@
+a = "Congratulations on winning the gold medal! Your hard work and dedication have truly paid off, and I couldn't be prouder of you. This is such a fantastic achievement, and you deserve all the recognition and applause.Well done, my friend! Here's to more success in the future!"
+b = a.upper()
+print(b)
