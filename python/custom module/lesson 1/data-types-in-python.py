f59d09df88980b3a3d2c9574ba75c9bebbc365b3
adarshani <adarshanipatra@github.com>
2025-02-01 20:40:53 +0530

    data types in python

---
+++ python/custom module/lesson 1/data-types-in-python.py
@@ -0,0 +1,12 @@
+#check for the data types of the following value
+a = 5
+print("data type of a is",type(a))
+
+b = 5.0
+print("data type of b is",type(b))
+
+c = True
+print("data type of c is",type(c))
+
+d = "Hello"
+print("data type of d is",type(d))
