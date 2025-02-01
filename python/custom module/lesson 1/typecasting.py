f3bbdd7ac933f6c1b65fa1f550cb917dc96c28bb
adarshani <adarshanipatra@github.com>
2025-02-01 21:03:02 +0530

    typecasting

---
+++ python/custom module/lesson 1/typecasting.py
@@ -0,0 +1,34 @@
+name = "Adu"
+age = 12
+percentage = 90.5
+is_student = True
+
+print("Name is",name)
+print("data type of name is",type(name))
+
+print("Age is",age)
+print("data type of age is",type(age))
+
+print("Percentage is",percentage)
+print("data type of percentage is",type(percentage))
+
+print("Is student is",is_student)
+print("data type of is_student is",type(is_student))
+
+print("\n after typecasting") 
+age = str(age)
+print(age)
+print("data type of age is",type(age))
+
+percentage = int(percentage)
+print(percentage)
+print("data type of percentage is",type(percentage))
+
+
+is_student = str(is_student)
+print(is_student)
+print("data type of is-student is",type(is_student))
+
+
+
+
