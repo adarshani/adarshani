90fff738b8bcc726a5af9b9df75333bff9c96510
adarshani <adarshanipatra@github.com>
2025-02-02 21:17:24 +0530

    aquareroot

---
+++ python/custom module/lesson_2/squareroot.py
@@ -0,0 +1,6 @@
+import math
+
+num = float(input("Enter a number: "))
+num_sqrt = math.sqrt(num)
+print("The square root of", num, "is", num_sqrt)
+
---
+++ python/custom module/lesson_2/year.py
@@ -0,0 +1,10 @@
+years = int(input("Enter the number of years: "))
+
+months = years * 12
+weeks = years * 52
+days = years * 365
+
+print("The number of months is", months)
+print("The number of weeks is", weeks)
+print("The number of days is", days)
+14
