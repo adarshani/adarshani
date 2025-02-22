64e1cba8ae6bda9ffb37c22a8c6370b5f1800d9a
adarshani <adarshanipatra@github.com>
2025-02-22 21:16:40 +0530

    lesson1

---
+++ python/custom module 2/lesson 1/armstrong.py
@@ -0,0 +1,17 @@
+num = int(input("enter a number"))
+
+sum = 0
+
+temp = num
+
+while temp>0:
+    digit = temp % 10
+    sum = sum+ digit**3
+    temp=temp//10
+
+if num == sum:
+    print("it is an armstrong number")
+
+else:
+    print("it is not an armstrong number")
+
