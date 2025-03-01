19d222a403da6dd0a85e2fb76c4221a95994a769
adarshani <adarshanipatra@github.com>
2025-03-01 21:27:40 +0530

    pattern

---
+++ python/custom module 3/lesson1/right-angle.py
@@ -0,0 +1,13 @@
+print("half pyramid pattern of stars")
+n = int(input('Enter the number of rows'))
+#for rows
+for i in range(n):
+    #for columns
+    for j in range(i+1):
+
+        print("*",end="")
+
+    print()
+
+
+
